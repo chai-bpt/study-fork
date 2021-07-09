d8c3641755d88338e557e0da458d7ca3a1d65bfb
chai-bpt <chai.bpt@gmail.com>
2021-07-09 19:11:12 +0530

    return value of fork() verified

--- test.c
+++ test.c
@@ -5,8 +5,14 @@
 
 int main(int argc, char* argv[])
 {
+	int iPid = 0;
+	
 	printf("\n\t[%d]\t%s\t%s\t%s\t%s\t%d\tSTART\n",getpid(),__DATE__,__TIME__,__FILE__,__func__,__LINE__);
-	fork();
+	
+	iPid = fork();
+
+	printf("\n\t[%d]\tiPid = %d\n",getpid(),iPid);
+
 	printf("\n\t[%d]\t%s\t%s\t%s\t%s\t%d\tEND\n",getpid(),__DATE__,__TIME__,__FILE__,__func__,__LINE__);
 
         exit(0);
