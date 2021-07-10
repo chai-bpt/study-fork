7469bf7bf504c0110e76b8b55a517f3315409856
chai-bpt <chai.bpt@gmail.com>
2021-07-10 13:00:26 +0530

    verifiaction of code section access from parent and child

--- test.c
+++ test.c
@@ -4,62 +4,38 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 
-int iGlobal = 90;
+int func(void)
+{
+        printf("\n\t[PPID=%d]\t[PID=%d]\tCOMMON\tIN_FUNC\n",getppid(),getpid());
+}
 
 int main(int argc, char* argv[])
 {
 	int iPid = 0;
-	int iLocal = 10;
-	
-	printf("\n\t[PPID=%d]\t[PID=%d]\tPARENT\tiGlobal = %d\n",getppid(),getpid(),iGlobal);
-	printf("\n\t[PPID=%d]\t[PID=%d]\tPARENT\t&iGlobal = %p\n",getppid(),getpid(),&iGlobal);
-	printf("\n\t[PPID=%d]\t[PID=%d]\tPARENT\tiLocal = %d\n",getppid(),getpid(),iLocal);
-	printf("\n\t[PPID=%d]\t[PID=%d]\tPARENT\t&iLocal = %p\n",getppid(),getpid(),&iLocal);
-	printf("\n");
+
+	printf("\n\t[PPID=%d]\t[PID=%d]\tPARENT\n",getppid(),getpid());
+	func();
 
 	printf("\n");
 	iPid = fork();//Copy on write (COW) for iPid
-
-	printf("\n\t[PPID=%d]\t[PID=%d]\tCOMMON\tiPid = %d\n",getppid(),getpid(),iPid);
-	iGlobal++;//COW
-	iLocal++;//COW
-       	printf("\n\t[PPID=%d]\t[PID=%d]\tCOMMON\tiGlobal = %d\n",getppid(),getpid(),iGlobal);
-        printf("\n\t[PPID=%d]\t[PID=%d]\tCOMMON\t&iGlobal = %p\n",getppid(),getpid(),&iGlobal);
-        printf("\n\t[PPID=%d]\t[PID=%d]\tCOMMON\tiLocal = %d\n",getppid(),getpid(),iLocal);
-        printf("\n\t[PPID=%d]\t[PID=%d]\tCOMMON\t&iLocal = %p\n",getppid(),getpid(),&iLocal);
-	printf("\n");
-		
+	
+	printf("\n\t[PPID=%d]\t[PID=%d]\tCOMMON\n",getppid(),getpid());
+	func();	
 
 	if(iPid == 0)
-	{
-	       	iGlobal++;
-        	iLocal++;
-        	printf("\n\t[PPID=%d]\t[PID=%d]\tCHILD\tiGlobal = %d\n",getppid(),getpid(),iGlobal);
-        	printf("\n\t[PPID=%d]\t[PID=%d]\tCHILD\t&iGlobal = %p\n",getppid(),getpid(),&iGlobal);
-        	printf("\n\t[PPID=%d]\t[PID=%d]\tCHILD\tiLocal = %d\n",getppid(),getpid(),iLocal);
-      	  	printf("\n\t[PPID=%d]\t[PID=%d]\tCHILD\t&iLocal = %p\n",getppid(),getpid(),&iLocal);
-		printf("\n");
+	{ 
+		printf("\n\t[PPID=%d]\t[PID=%d]\tCHILD\n",getppid(),getpid());
+		func();
 	}
 	else
 	{	
-		iGlobal++;
-                iLocal++;
-                printf("\n\t[PPID=%d]\t[PID=%d]\tPARENT\tiGlobal = %d\n",getppid(),getpid(),iGlobal);
-                printf("\n\t[PPID=%d]\t[PID=%d]\tPARENT\t&iGlobal = %p\n",getppid(),getpid(),&iGlobal);
-                printf("\n\t[PPID=%d]\t[PID=%d]\tPARENT\tiLocal = %d\n",getppid(),getpid(),iLocal);
-	        printf("\n\t[PPID=%d]\t[PID=%d]\tPARENT\t&iLocal = %p\n",getppid(),getpid(),&iLocal);
-		printf("\n");
-	}
-
-        iGlobal++;
-        iLocal++;
-        printf("\n\t[PPID=%d]\t[PID=%d]\tCOMMON\tiGlobal = %d\n",getppid(),getpid(),iGlobal);
-        printf("\n\t[PPID=%d]\t[PID=%d]\tCOMMON\t&iGlobal = %p\n",getppid(),getpid(),&iGlobal);
-        printf("\n\t[PPID=%d]\t[PID=%d]\tCOMMON\tiLocal = %d\n",getppid(),getpid(),iLocal);
-        printf("\n\t[PPID=%d]\t[PID=%d]\tCOMMON\t&iLocal = %p\n",getppid(),getpid(),&iLocal);
-	printf("\n");
+		printf("\n\t[PPID=%d]\t[PID=%d]\tPARENT\n",getppid(),getpid());
+		func();
+	}    
 	
+	printf("\n\t[PPID=%d]\t[PID=%d]\tCOMMON\n",getppid(),getpid());
+	func();	
 
-        printf("\n\t[PPID=%d]\t[PID=%d]\t\tEXIT\n",getppid(),getpid());
+        printf("\n\t[PPID=%d]\t[PID=%d]\tCOMMON\tEXIT\n",getppid(),getpid());
         exit(0);
 }
