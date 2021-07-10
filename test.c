023a93d68a5cf6672f87c07c549b8c70b8e56a11
chai-bpt <chai.bpt@gmail.com>
2021-07-10 19:41:31 +0530

    verifiaction of file discriptor do not get copied on read

--- test.c
+++ test.c
@@ -3,31 +3,35 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int main(int argc, char* argv[])
 {
 	int iPid = 0;
+	int fd = 0;
+	char byte = 0;
 
         printf("\n\t[PPID=%d]\t[PID=%d]\tCOMMON\tSTART\n",getppid(),getpid());
+
+	fd = open("./datafile",0,O_RDONLY); 
 
 	printf("\n");
 	iPid = fork();
 	if(iPid == 0)
-	{ 
+	{
 		printf("\n\t[PPID=%d]\t[PID=%d]\tCHILD\n",getppid(),getpid());
-		goto parent_code;		
+		read(fd, &byte, 1);
+		printf("\n\t[PPID=%d]\t[PID=%d]\tCHILD\tbyte = %c\n",getppid(),getpid(),byte);
 	}
 	else
 	{	
+		sleep(1);
 		printf("\n\t[PPID=%d]\t[PID=%d]\tPARENT\n",getppid(),getpid());
-                if(0)
-                {
-parent_code:
-                        printf("\n\t[PPID=%d]\t[PID=%d]\tPARENT CODE::JUMP SUCCESS\n",getppid(),getpid());
-                        goto exit;
-                }
+		read(fd, &byte, 1);
+		printf("\n\t[PPID=%d]\t[PID=%d]\tPARENT\tbyte = %c\n",getppid(),getpid(),byte);
 	}    
-exit:
+
         printf("\n\t[PPID=%d]\t[PID=%d]\tCOMMON\tEXIT\n",getppid(),getpid());
         exit(0);
 }
