8e0cdb6ce88e75e2282a130fb7d8fd64df97c4b2
chai-bpt <chai.bpt@gmail.com>
2021-07-10 09:06:08 +0530

    collection of exit status when exit by signal

--- test.c
+++ test.c
@@ -4,20 +4,29 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 
+int terminate = 0;
 void handler()
 {
 	int exitStatus = 0;	
 	
 	printf("\n\t[PPID=%d]\t[PID=%d]\thandler\n",getppid(),getpid());
 	
+	sleep(20);
+
 	wait(&exitStatus);
 	
 	if(WIFEXITED(exitStatus))
 	{
 		printf("\n\t[PPID=%d]\t[PID=%d]\texitValue = %d\n",getppid(),getpid(),WEXITSTATUS(exitStatus));
 	}
+	else if(WIFSIGNALED(exitStatus))
+	{
+		printf("\n\t[PPID=%d]\t[PID=%d]\tsignalValue = %d\n",getppid(),getpid(),WTERMSIG(exitStatus));
+	}
 
 	printf("\n\t[PPID=%d]\t[PID=%d]\thandler :: RECEIVED CHILD EXIT\n",getppid(),getpid());
+
+	terminate = 1;
 }
 
 int main(int argc, char* argv[])
@@ -34,27 +43,31 @@ int main(int argc, char* argv[])
 	{
 		printf("\n\t[PPID=%d]\t[PID=%d]\tCHILD\n",getppid(),getpid());
 	
-		sleep(10);
+		printf("\n\t[PPID=%d]\t[PID=%d]\tKILL CHILD\n",getppid(),getpid());
 
-		printf("\n\t[PPID=%d]\t[PID=%d]\tCHILD-EXITING\n",getppid(),getpid());
+		while(1)
+		{
+			sleep(1);
+		}
 
-		exit(100);
 	}
 	else
 	{	
 		printf("\n\t[PPID=%d]\t[PID=%d]\tPARENT\n",getppid(),getpid());
 		
-		sleep(5);
-
 		printf("\n\t[PPID=%d]\t[PID=%d]\tPARENT WATING FOR CHILD\n",getppid(),getpid());
 		
 		signal (SIGCHLD, handler);
-
-		sleep(15);
 		
-		sleep(10);
+		while(terminate == 0)
+		{
+			sleep(1);		
+			sleep(1);
+		}
 
 		printf("\n\t[PPID=%d]\t[PID=%d]\tPARENT-EXITING\n",getppid(),getpid());
+
+		sleep(10);
 
 	}
 
